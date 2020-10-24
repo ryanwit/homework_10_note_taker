5b0f4929769b53dadb53c6e6e1adea390c72885b
Ryan Witherspoon <ry.witherspoon@gmail.com>
2020-10-24 16:55:52 -0600

    Updated server file

--- server.js
+++ server.js
@@ -1,29 +1,24 @@
 //  add in all paths required
 const express = require("express");
-const app = express();
 const apiRoutes = require("./routes/apiroutes")
 const htmlRoutes = require("./routes/htmlroutes")
 
-// create PORT to use
+
+const app = express();
 const PORT = 3000;
-app.use(express.json());
+
+
+
+// create PORT to use
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public")); // server once you start it goes into public folder and looks for html file
-app.use("/api", apiRoutes); 
-app.use("/", htmlRoutes);
+
+app.use(apiRoutes); 
+app.use(htmlRoutes);
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
 
 
-// create a routes to use
-// app.get("/notes", function(req, res){
-//     res.sendFile(path.join(__direname, "./publ"))
-// })
-
-// Create function for getting all notes
-
-//  Create function for saving a note
-
-// Create function for deleting a note
 
 
