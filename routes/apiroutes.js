51055d478bfada3a0931a0a0bef3637280fd04f3
Ryan Witherspoon <ry.witherspoon@gmail.com>
2020-10-24 13:34:48 -0600

    Update API Routes

---
+++ routes/apiroutes.js
@@ -0,0 +1,33 @@
+const router = require("express").Router()
+const notes = require("../db/notesclass")
+
+router.get("/api/notes", function(req, res){
+    notes.getNotes()
+    .then((data){
+        return res.json(data)
+    }).catch(err => {
+        return res.json(err)
+    })
+})
+
+router.post("/api/notes", function(req, res){
+    notes.addNotes(req.body)
+    .then((data) => {
+        return res.json(data)
+    }).catch(err => {
+        return res.json(err)
+    })
+})
+
+router.delete("/api/notes/:id", function(req, res){
+    notes.deleteNotes(req.params.id)
+    .then(() => {
+        return res.json({
+            ok: true
+        })
+    }).catch(err => {
+        return res.json(err)
+    })
+})
+
+module.exports = router
