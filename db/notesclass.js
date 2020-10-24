da69e55fd7f83fd3e9bac629eeb33f6d1601df0a
Ryan Witherspoon <ry.witherspoon@gmail.com>
2020-10-24 14:13:00 -0600

    Working on add note function

--- db/notesclass.js
+++ db/notesclass.js
@@ -9,6 +9,49 @@ class Notes {
         return readFileAsync("db/db.json", "utf8")
     }
     writeNotes(data) {
-        return writeFileAsync("db/db.json", "utf8")
+        return writeFileAsync("db/db.json", JSON.stringify(data))
     }
+    getNotes(){
+        return this.readNotes()
+        .then(data => {
+            let notes;
+
+            try{
+                notes = [].concat(JSON.parse(data))
+            }
+
+            catch(err) {
+                notes = [];
+            }
+
+            return notes
+        })
+    }
+    addNotes(data) {
+        const {title, text} = data
+        if(!title || !text) {
+            throw new Error("Please enter a title and text below!")
+        }
+        const competedNote = {title, text, id:id++}
+
+        return this.getNotes()
+        .then(data => {
+            return[...data,completedNote]
+        }).then(data => {
+            return this.writeNotes(data)
+        }).then(() => {
+            return completedNote
+        })
+    }
+
+    // deletedNotes(id) {
+    //     return this.getNotes()
+    //     .then(data => {
+    //         return data.filter(notes => notes.id !== id)
+    //     }).then(data => {
+    //         return this.writeNotes(data)
+    //     })
+    // }
 }
+
+
