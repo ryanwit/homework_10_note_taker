f555639c2667c74d619fa365eaaa5f3ffe972320
Ryan Witherspoon <ry.witherspoon@gmail.com>
2020-10-25 13:26:26 -0600

    Update

--- Develop/routes/htmlroutes.js
+++ Develop/routes/htmlroutes.js
@@ -3,12 +3,16 @@
 /* -------------------------------------------------------------------------- */
 
 const path = require("path")
-const router = require("express").Router()
+const router = require("express").Router()  //stream line way to do routes
 
+/* ------------------------ TAKES USERS TO NOTES PAGE ----------------------- */
 
 router.get("/notes", function(req, res){
     res.sendFile(path.join(__dirname, "../public/notes.html"))
 })
+
+/* --------------------------- STAR PATH CATCH ALL -------------------------- */
+
 router.get("*", function(req, res){
     res.sendFile(path.join(__dirname, "../public/index.html"))
 })
--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,20 @@
-//  add in all paths required
+/* ------------------------ add in all paths required ----------------------- */
 const express = require("express");
 const apiRoutes = require("./routes/apiroutes")
 const htmlRoutes = require("./routes/htmlroutes")
-// create PORT to use
+
+/* --------------------------- create PORT to use --------------------------- */
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public")); // server once you start it goes into public folder and looks for html file
+
+/* ------------------------------ middle layer ------------------------------ */
+app.use(express.urlencoded({ extended: true })); // allows easy access to info from the front end to the back end
+app.use(express.json()); // giving access to req.body
+app.use(express.static("public")); // server once you start it goes into public folder first and looks for index.html file
 app.use("/api", apiRoutes); 
-app.use("/", htmlRoutes);
-// Start the server on the port
+app.use("/", htmlRoutes); 
+
+/* -------------------------------- listener -------------------------------- */
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
 
 
